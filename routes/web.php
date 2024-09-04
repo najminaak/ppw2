75c1ede83e39cf97438b2761d2e88afe6b1dece6
najminaak <najminakinanti2@gmail.com>
2024-09-04 10:02:59 +0700

    ppw 2

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "anti",
+        "email" => "najminakinantiputri@mail.ugm.ac.id"
+        ]);
+});
+
+Route::get('/bbblade', function () {
+    return view('bbblade');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about2', function () {
+    return view('about2');
+});
+
+Route::get('/posts', [PostController::class, 'index']);
