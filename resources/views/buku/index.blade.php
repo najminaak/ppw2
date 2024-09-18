97644d6abf269209ef082d8be8f26e10e43e2c98
najminaak <najminakinanti2@gmail.com>
2024-09-18 22:27:20 +0700

    CRUD

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -4,18 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Buku</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
+        body {
+            padding: 50px;
+        }
         table {
-            width: 100%;
             border-collapse: collapse;
         }
 
+        th, td {
+            padding: 8px;  
+        }
+
+        thead th {
+            background-color: #007bff; 
+            color: white;
+        }
+        
         table, th, td {
-            border: 1px solid black;
+            border: 1px solid #dee2e6; 
+        }
+
+        .d-flex {
+            display: flex;
+            align-items: center;
         }
     </style>
 </head>
 <body>
+    <h2 class="text-center">Data Buku</h2>
     <table class="table">
         <thead>
             <tr>
@@ -37,7 +55,20 @@
                     <td>{{ $buku->penulis }}</td>
                     <td>{{ "Rp ".number_format($buku->harga, 2, ',', '.') }}</td>
                     <td>{{ \Carbon\Carbon::parse($buku->tgl_terbit)->format('d-m-Y') }}</td>
-                    <td></td>
+                    <td>
+                        <div class="d-flex align-items-center">
+                            <form action="{{ route('buku.edit', $buku->id) }}" class="mr-2">
+                                @csrf 
+                                <button type="submit" class="btn btn-warning">Edit</button>
+                            </form>
+                            <form action="{{ route('buku.destroy', $buku->id) }}" method="POST">
+                                @csrf 
+                                @method('DELETE')
+                                <button onclick="return confirm('Yakin mau dihapus??')" 
+                                type="submit" class="btn btn-danger">Hapus</button>
+                            </form>
+                        </div>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
@@ -48,5 +79,8 @@
     
     <!-- menampilkan total harga buku -->
     <p>Total Harga Buku: Rp {{ number_format($total_harga, 2, ',', '.') }}</p>
+    
+    <!-- membuat tombol tambah buku -->
+    <a href="{{ route('buku.create') }}" class="btn btn-primary"> Tambah Buku</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,9 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::get('/article', [ArticleController::class, 'index']);
 
 Route::get('/buku', [BukuController::class, 'index']);
+Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
+Route::post('/buku', [BukuController::class, 'store'])->name('buku.store');
+Route::delete('/buku/{id}', [BukuController::class, 'destroy'])->name('buku.destroy');
+
+Route::get('/buku/{id}/edit', [BukuController::class, 'edit'])->name('buku.edit');
+Route::put('/buku/{id}', [BukuController::class, 'update'])->name('buku.update');
